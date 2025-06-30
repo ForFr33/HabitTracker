2db9cc0db461e514700ef54daf426b979b74046a
Ole Andersen <olejonas6@gmail.com>
2025-06-30 15:44:53 +0200

    first commit, no real changes

---
+++ script.js
@@ -0,0 +1,22 @@
+class Habit {
+    name;
+    date;
+    habitIsDone;
+
+    constructor(name, date, habitIsDone) {
+        this.name = name;
+        this.date = date;
+        this.habitIsDone = false;
+    }
+
+
+}
+
+class Table {
+}
+
+let btn1 = document.getElementsById("btn1");
+
+function addHabit() {
+    
+}
